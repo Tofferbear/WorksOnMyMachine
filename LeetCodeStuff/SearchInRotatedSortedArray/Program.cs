0f8f3c52c295f02d0ba26623a68b7569ca918c87
TofferAtFractal <toffer.davidson@fractal.ai>
2025-07-31 11:17:20 -0700

    initial adding of search in rotated sorted array

---
+++ LeetCodeStuff/SearchInRotatedSortedArray/Program.cs
@@ -0,0 +1,18 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Search(new[] { 4, 5, 6, 7, 0, 1, 2 }, 0)); // should return 4
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; ++i)
+        {
+            if (nums[i] == target)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
