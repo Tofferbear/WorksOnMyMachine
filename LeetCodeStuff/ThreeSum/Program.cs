82417582da8f2a825566250ea4d266559672e6b7
TofferAtFractal <toffer.davidson@fractal.ai>
2025-08-28 21:44:01 -0700

    add new solution for Three Sum challenge

---
+++ LeetCodeStuff/ThreeSum/Program.cs
@@ -0,0 +1,53 @@
+﻿var solution = new Solution();
+
+var nums = new int[] { -1, 0, 1, 2, -1, -4 };
+var result = solution.ThreeSum(nums);
+
+// should be [[-1,-1,2],[-1,0,1]]
+foreach (var triplet in result)
+{
+    Console.WriteLine($"[{string.Join(", ", triplet)}]");
+}
+
+public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums);
+        var result = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0)
+                {
+                    result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
